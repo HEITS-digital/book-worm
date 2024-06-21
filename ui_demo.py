59b15554501ac6aa765c29bc3e73b82e1934efad
Giani <giani.statie@heits.digital>
2024-06-21 17:48:42 +0300

    feat: added code that lets you run bookworm tools

--- ui_demo.py
+++ ui_demo.py
@@ -1,8 +1,7 @@
 import time
 import gradio as gr
 
-from chat import Chat
-from itertools import chain
+from bookwom import BookWorm
 
 
 # def echo(message, history):
@@ -22,30 +21,42 @@ from itertools import chain
 #     for i in generator3:
 #         yield i
 
-def echo(message, history):
-    # need to find a better way to instantiate this, BC it cannot be declared globally :(
-    chat = Chat()
-    response = chat.parse_message(message)
-    for i in range(len(response)):
-        time.sleep(0.02)
-        yield response[: i+1]
 
+def update_env_vars(env_file_path: str = None):
+    import os
+    from dotenv import dotenv_values
 
-demo = gr.ChatInterface(
-    echo,
-    chatbot=gr.Chatbot(height=300),
-    textbox=gr.Textbox(placeholder="Ask me about a book", container=False, scale=7),
-    title="Book Worm",
-    description="Your AI librarian for classical literature",
-    examples=[
-        "Do you know the book Guliver's Travels?",
-        "What was the name of the first island that Guliver got to?"
-    ],
-    cache_examples=False,
-    retry_btn=None,
-    undo_btn="Delete Previous",
-    clear_btn="Clear",
-)
+    if env_file_path:
+        env_config = dotenv_values(env_file_path)
+        os.environ = {**os.environ, **env_config}
+
 
 if __name__ == "__main__":
+    update_env_vars(".env")
+
+    def echo(message, history):
+        # need to find a better way to instantiate this, BC it cannot be declared globally :(
+        chat = BookWorm()
+        response = chat.ask_bookworm(message)
+        output = response["output"]
+        for i in range(len(output)):
+            time.sleep(0.02)
+            yield output[: i + 1]
+
+    demo = gr.ChatInterface(
+        echo,
+        chatbot=gr.Chatbot(height=300),
+        textbox=gr.Textbox(placeholder="Ask me about a book", container=False, scale=7),
+        title="Book Worm",
+        description="Your AI librarian for classical literature",
+        examples=[
+            "Do you know the book Guliver's Travels?",
+            "What was the name of the first island that Guliver got to?",
+        ],
+        cache_examples=False,
+        retry_btn=None,
+        undo_btn="Delete Previous",
+        clear_btn="Clear",
+    )
+
     demo.launch(share=False)
