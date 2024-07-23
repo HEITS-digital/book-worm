680fb263db0ee1ed51d229d129800b0e8363d460
GianiHeits <giani.statie@heits.digital>
2024-07-23 09:36:36 +0300

    cleaned up the code a bit

--- ui_demo.py
+++ ui_demo.py
@@ -1,6 +1,4 @@
-import uuid
 import time
-
 import gradio as gr
 
 from book_worm import BookWorm
@@ -17,9 +15,8 @@ def update_env_vars(env_file_path: str = None):
 
 if __name__ == "__main__":
     update_env_vars(".env")
-    session_id = str(uuid.uuid4())
 
-    def echo(message, history, session_id):
+    def echo(message, history):
         # need to find a better way to instantiate this, BC it cannot be declared globally :(
         chat = BookWorm(history)
         response = chat.ask_bookworm(message)
@@ -34,12 +31,9 @@ if __name__ == "__main__":
         textbox=gr.Textbox(placeholder="Ask me about a book", container=False, scale=7),
         title="Book Worm",
         description="Your AI librarian for classical literature",
-        additional_inputs=[
-            gr.Textbox(session_id, label="Session ID"),
-        ],
         examples=[
-            ["Do you know the book Guliver's Travels?", session_id],
-            ["What was the name of the first island that Guliver got to?", session_id],
+            ["Do you know the book Guliver's Travels?"],
+            ["What was the name of the first island that Guliver got to?"],
         ],
         cache_examples=False,
         retry_btn=None,
@@ -48,4 +42,4 @@ if __name__ == "__main__":
         concurrency_limit=10,
     )
 
-    demo.launch(share=True, max_threads=40)
+    demo.launch(share=True)
