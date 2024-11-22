c8055477d09a97c6aee2a0b24bb7a840bc311464
GianiHeits <giani.statie@heits.digital>
2024-11-22 14:07:09 +0200

    added agent pool

--- app/bookworm/views.py
+++ app/bookworm/views.py
@@ -1,19 +1,21 @@
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
-from .scripts.agent import Agent
+from .scripts.agent_pool import AgentPool
 
 # TODO: create agent pool
-agent = Agent()
+agent_pool = AgentPool()
 
 
 def ask_bookworm(request):
     if request.method == "POST":
-        print(request.POST)
         message = request.POST.get("message", None)
         if not message:
             return JsonResponse({"error": "Invalid input: 'message' is required."}, status=400)
 
+        agent = agent_pool.acquire()
         data = agent.ask_bookworm(message)
+        agent_pool.release(agent)
+
         return JsonResponse({"answer": data["output"]}, safe=False)  # Return data as JSON response
 
 
